04adef3ce2631db041bd5ad53a434d21e921e7be
kevinseabourne <kevinseabourne@hotmail.com>
2021-08-02 08:53:19 +0800

    added svg support for local files

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ module.exports = {
     domains: ["image.tmdb.org", "www.themoviedb.org", "chpistel.sirv.com"],
   },
   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|mp3|aif|svg)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "static/media/[name].[hash:8].[ext]",
+          },
+        },
+      ],
+    });
+
     return config;
   },
 };
