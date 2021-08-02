0a04745f0453b2392d51597d24e8ab921cd4fb81
kevinseabourne <kevinseabourne@hotmail.com>
2021-08-02 08:55:28 +0800

    updated favourites page

--- pages/favourites.js
+++ pages/favourites.js
@@ -1,14 +1,50 @@
+import { useState, useEffect } from "react";
+import { isArrayEmpty } from "../components/common/utils/isEmpty";
 import Movies from "../components/common/movies";
 import styled from "styled-components";
 
-const Favourites = ({ favMovies }) => {
+const Favourites = () => {
+  const [favMovies, setFavMovies] = useState([]);
+  useEffect(() => {
+    const favMovies = localStorage.getItem("favouriteMovies")
+      ? JSON.parse(localStorage.getItem("favouriteMovies"))
+      : [];
+    isArrayEmpty(favMovies) && setFavMovies(favMovies);
+  }, []);
+
   return (
     <Container>
-      <Movies movies={favMovies} />
+      {isArrayEmpty(favMovies) ? (
+        <Movies movies={favMovies} />
+      ) : (
+        <TitleContainer>
+          <Title>No Favourite Movies...</Title>
+          <SubTitle>Go find them !</SubTitle>
+        </TitleContainer>
+      )}
     </Container>
   );
 };
 
 export default Favourites;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  margin-top: 50px;
+`;
+
+const SubTitle = styled.h2`
+  margin-top: 5px;
+`;
