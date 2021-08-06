3b4149f36a98cd614a97b44083c0c32988105ea8
kevinseabourne <kevinseabourne@hotmail.com>
2021-08-06 08:49:46 +0800

    updated

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,36 @@
 import styled from "styled-components";
 
 const About = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Title>About</Title>
+      <Description>
+        Watch trailer's and save the movies you love. In the favourite's
+        section, (Please note when clearing browser data. Your favourite's data
+        may be deleted as is it stored in the browser) All data from this site
+        is provided by the Movie DB.
+      </Description>
+    </Container>
+  );
 };
 
 export default About;
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  margin-top: 60px;
+`;
+
+const Description = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+  max-width: 840px;
+  line-height: 1.8;
+`;
