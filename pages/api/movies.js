da92afa8cef51630ba12ee047804b9aff399b770
kevinseabourne <kevinseabourne@hotmail.com>
2021-08-10 07:15:52 +0800

    refactored api calls

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -1,93 +1,77 @@
 import http from "./httpService";
 
-const apiEndPoint = `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.NEXT_PUBLIC_MOVIES_DB_API_KEY}&append_to_response=videos&page=`;
-
-function movieUrl(id) {
-  return `${apiEndPoint}/${id}`;
-}
-
 export async function getTrendingMovies(page) {
-  let promises = [];
+  let movies = [];
   for (let i = page; i <= page + 2; i++) {
     let { data } = await http.get(
-      `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.NEXT_PUBLIC_MOVIES_DB_API_KEY}&page=${i}&vote_average.gte=1`
+      `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.NEXT_PUBLIC_MOVIES_DB_API_KEY}&page=${i}&include_adult=false`
+    );
+    const results = data.results;
+    results.map((movie) => {
+      return movies.push(movie);
+    });
+  }
+  // // This is adding a property to each movie object.
+  // const allMovies = movies.map((obj) => {
+  //   obj["favourite"] = false;
+  //   return obj;
+  // });
+
+  // Only show movies that have been released
+  const filteredMovies = movies.filter((movie) => {
+    return movie.vote_average !== 0;
+  });
+  return filteredMovies;
+}
+
+export async function getMovies(page, genreId, sortBy) {
+  const genreQuery = genreId ? `&with_genres=${genreId}` : "&";
+  const sortByQuery = sortBy ? `&sort_by=${sortBy}` : "&";
+  let movies = [];
+  for (let i = page; i <= page + 2; i++) {
+    let { data } = await http.get(
+      `https://api.themoviedb.org/3/discover/movie/?api_key=${process.env.NEXT_PUBLIC_MOVIES_DB_API_KEY}&language=en-US${sortByQuery}&include_adult=false${genreQuery}&vote_average.gte=1&vote_count.gte=2200&page=${i}`
+    );
+    const results = data.results;
+    results.map((movie) => {
+      return movies.push(movie);
+    });
+  }
+  return movies;
+}
+
+export async function getGenreMovies(page, genreId, sortBy) {
+  const genreQuery = genreId ? `&with_genres=${genreId}` : "&";
+  const sortByQuery = sortBy ? `&sort_by=${sortBy}` : "&";
+  let movies = [];
+  for (let i = page; i <= page + 2; i++) {
+    let { data } = await http.get(
+      `https://api.themoviedb.org/3/discover/movie/?api_key=${process.env.NEXT_PUBLIC_MOVIES_DB_API_KEY}&language=en-US${sortByQuery}&include_adult=false${genreQuery}&vote_average.gte=1&vote_count.gte=2200&page=${i}`
     );
     let moviesArray = data.results;
     moviesArray.map((obj) => {
-      return promises.push(obj);
+      return movies.push(obj);
     });
   }
-  // This is adding a property to each movie object.
-  const allMovies = promises.map((obj) => {
-    obj["favourite"] = false;
-    return obj;
-  });
-  // Only Show movies that are released
-  // const movies = allMovies.filter((movie) => {
-  //   return movie.vote_average !== 0;
+  // // This is adding a property to each movie object.
+  // const allMovies = promises.map((obj) => {
+  //   obj["favourite"] = false;
+  //   return obj;
   // });
   return allMovies;
 }
 
-export async function getPopularOrTopRatedMovies(page, sortBy) {
-  let promises = [];
-  for (let i = page; i <= page + 2; i++) {
-    let { data } = await http.get(
-      `https://api.themoviedb.org/3/movie/${sortBy}?api_key=${process.env.NEXT_PUBLIC_MOVIES_DB_API_KEY}&language=en-US&page=${i}&vote_average.gte=1`
-    );
-    let moviesArray = data.results;
-    moviesArray.map((obj) => {
-      return promises.push(obj);
-    });
-  }
-  // This is adding a property to each movie object.
-  const allMovies = promises.map((obj) => {
-    obj["favourite"] = false;
-    return obj;
-  });
-  // Only Show movies that are released
-  const movies = allMovies.filter((movie) => {
-    return movie.vote_average !== 0;
-  });
-  return movies;
-}
-
-export async function getGenreMovies(page, genreId) {
-  let promises = [];
-  for (let i = page; i <= page + 2; i++) {
-    let { data } = await http.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_DB_API_KEY}&page=${i}&with_genres=${genreId}&vote_average.gte=1`
-    );
-    let moviesArray = data.results;
-    moviesArray.map((obj) => {
-      return promises.push(obj);
-    });
-  }
-  // This is adding a property to each movie object.
-  const allMovies = promises.map((obj) => {
-    obj["favourite"] = false;
-    return obj;
-  });
-  // Only Show movies that are released
-  const movies = allMovies.filter((movie) => {
-    return movie.vote_average !== 0;
-  });
-  return movies;
-}
-
 export async function textSearchMovies(query) {
   let { data } = await http.get(
-    `http://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_DB_API_KEY}&query=${query}`
+    `http://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_MOVIES_DB_API_KEY}&query=${query}&include_adult=false`
   );
 
   let moviesArray = data.results;
-  // Only Show movies that are released
+
+  // Only Show movies have a vote count higher that 300
   const movies = moviesArray.filter((movie) => {
     return movie.vote_count > 300;
   });
-
-  // movies are sorted in descending order based on the vote average
-  movies.sort((a, b) => b.vote_average - a.vote_average);
 
   return movies;
 }
