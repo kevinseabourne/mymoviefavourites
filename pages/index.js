97d2200feab466a95f2b042990039bcf7fe00f5a
kevinseabourne <kevinseabourne@hotmail.com>
2021-08-27 00:06:14 +0800

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,6 @@ import Movies from "../components/common/movies";
 import AppContext from "../context/appContext";
 import styled from "styled-components";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`;
-
 export default function Home() {
   const {
     movies,
